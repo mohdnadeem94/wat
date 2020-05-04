360e53442f4ff7c4404a24a2bd1ba394af991ea9
mohdnadeem94 <nadeemneu.com>
2020-05-04 17:09:53 -0400

    8th_commit

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
+amazon_url ='https://www.amazon.com/s?k={sear}&ref=nb_sb_noss_2'
+walmart_url = 'https://www.walmart.com/search/?query={sear}'
+target_url = 'https://www.target.com/s?searchTerm={sear}'
+
 # Create your views here.
 def AppPage(request):
     return render(request,'app_name.html')
